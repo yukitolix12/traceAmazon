5cabb504e74e1c950d69a3722e313b6be665f0b9
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-13 12:48:19 +0900

    amazonのURL貼り付け

--- amazonTracker.py
+++ amazonTracker.py
@@ -1,2 +1,4 @@
 import requests
 from bs4 import BeautifulSoup
+
+amazonURL = "https://www.amazon.co.jp/Python%E3%81%A7%E3%81%AF%E3%81%98%E3%82%81%E3%82%8B%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92-%E2%80%95scikit-learn%E3%81%A7%E5%AD%A6%E3%81%B6%E7%89%B9%E5%BE%B4%E9%87%8F%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%A8%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E3%81%AE%E5%9F%BA%E7%A4%8E-Andreas-C-Muller/dp/4873117984/ref=asc_df_4873117984/?tag=jpgo-22&linkCode=df0&hvadid=295686767484&hvpos=&hvnetw=g&hvrand=566347388621549223&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009482&hvtargid=pla-526272655673&psc=1&th=1&psc=1"
